d417f72fbfd8c639b626fc85ed7dfebcf3c6454a
Marcin Urban <marcin@fieldly.com>
2020-02-06 13:55:08 +0100

    postgres swithc

--- db/schema.rb
+++ db/schema.rb
@@ -13,28 +13,30 @@
 
 ActiveRecord::Schema.define(version: 20200123073919) do
 
-  create_table "event_store_events", id: false, force: :cascade do |t|
-    t.string   "id",         limit: 36
-    t.string   "event_type",            null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "pgcrypto"
+
+  create_table "event_store_events", id: :uuid, default: "gen_random_uuid()", force: :cascade do |t|
+    t.string   "event_type", null: false
     t.binary   "metadata"
-    t.binary   "data",                  null: false
-    t.datetime "created_at",            null: false
+    t.binary   "data",       null: false
+    t.datetime "created_at", null: false
   end
 
-  add_index "event_store_events", ["created_at"], name: "index_event_store_events_on_created_at"
-  add_index "event_store_events", ["event_type"], name: "index_event_store_events_on_event_type"
-  add_index "event_store_events", ["id"], name: "index_event_store_events_on_id", unique: true
+  add_index "event_store_events", ["created_at"], name: "index_event_store_events_on_created_at", using: :btree
+  add_index "event_store_events", ["event_type"], name: "index_event_store_events_on_event_type", using: :btree
 
   create_table "event_store_events_in_streams", force: :cascade do |t|
-    t.string   "stream",                null: false
+    t.string   "stream",     null: false
     t.integer  "position"
-    t.string   "event_id",   limit: 36, null: false
-    t.datetime "created_at",            null: false
+    t.uuid     "event_id",   null: false
+    t.datetime "created_at", null: false
   end
 
-  add_index "event_store_events_in_streams", ["created_at"], name: "index_event_store_events_in_streams_on_created_at"
-  add_index "event_store_events_in_streams", ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true
-  add_index "event_store_events_in_streams", ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true
+  add_index "event_store_events_in_streams", ["created_at"], name: "index_event_store_events_in_streams_on_created_at", using: :btree
+  add_index "event_store_events_in_streams", ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true, using: :btree
+  add_index "event_store_events_in_streams", ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true, using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
